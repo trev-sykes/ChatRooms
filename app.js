f291cdf001903a93fd6a39a30e26804b72584307
trev <trevorjacobsykes@gmail.com>
2025-12-08 21:24:27 -0800

    add new domain to cors

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors({
     origin: [
         "https://chat-rooms-chi.vercel.app",
+        "https://chatrooms-connect.vercel.app",
         "http://localhost:5173",
         "http://localhost:5174"
     ],
