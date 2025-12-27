65dc9ca9307c59279f5725e26d073568cdac558e
trev <trevorjacobsykes@gmail.com>
2025-12-26 16:34:22 -0800

    update footer

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -100,7 +100,6 @@ export const Landing: React.FC = () => {
                     >
                         Source Code
                     </a>
-                    • Happy Coding
                 </CardFooter>
             </Card>
         </PageWrapper>
